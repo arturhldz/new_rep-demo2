bb9dd79e981a25b21960feca95f0bb743df88a37
Arturo <arturhldz1997@gmail.com>
2022-09-29 16:06:23 -0500

    Calculadora en c

---
+++ Calculadora.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int suma(int num1,int num2){
+    return num1 + num2;
+}
+
+int main(int argc, char const *argv[])
+{
+    int num1,num2;
+
+    printf("Ingrese el numero 1 : \n");
+    scanf("%d",&num1);
+    printf("Ingrese el numero 2 : \n");
+    scanf("%d",&num2);
+
+    printf("El resutlado de la suma es : %d \n",suma(num1,num2));
+    
+    return 0;
+}
