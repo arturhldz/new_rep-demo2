a62c5af8b7f87a393359fdfabe10979c63fe711f
Arturo <arturhldz1997@gmail.com>
2022-09-29 18:30:31 -0500

    Se agrego la funcion de dividir

--- Calculadora.c
+++ Calculadora.c
@@ -15,6 +15,9 @@ int resta(int num1,int num2){
 int multi(int num1,int num2){
     return num1 * num2;
 }
+int div(int num1,int num2){
+    return num1 /num2;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -28,7 +31,7 @@ int main(int argc, char const *argv[])
     printf("El resutlado de la suma es : %d \n",suma(num1,num2));
     printf("El resutlado de la resta es : %d \n",resta(num1,num2));
     printf("El resutlado de la multiplicacion es : %d \n",multi(num1,num2));
-
+    printf("El resultado de la divicion es : %d \n",div(num1,num2));
     
     return 0;
 }
